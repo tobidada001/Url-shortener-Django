2882832c21383807a92f6b6e104167f0a65b2839
OLUWATOBI <dadaoluwatobijohn@gmail.com>
2023-06-28 02:45:09 +0100

    I'm ready to deploy my url shortner app

--- Shortener/views.py
+++ Shortener/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.sites.shortcuts import get_current_site
 from django.views.generic import View
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import ShortenedUrl
 from urllib.parse import urlparse
 import uuid
 # Create your views here.
-
-domain = 'localhost:8000/'
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return render(request, template_name='index.html')
 
     def post(self, request, *args, **kwargs):
-        url = request.POST['url']
-        data = ShortenedUrl.objects.create(url = url, short_url = str(uuid.uuid4())[:8])
-        return HttpResponse(domain + data.short_url)
+        url = request.POST.get('url')
+        if url:
+            data = ShortenedUrl.objects.create(url = url, short_url = str(uuid.uuid4())[:8])
+            print(str(get_current_site(request)) + data.short_url)
+            return HttpResponse(str(get_current_site(request)) +'/'+ data.short_url)
+        else:
+            return JsonResponse({ 'error' : 'Url field must not be empty.'})
 
 
 def redirect_to_link(request, part):
--- UrlShortener/settings.py
+++ UrlShortener/settings.py
@@ -1,17 +1,16 @@
 
 from pathlib import Path
-from decouple import config
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", 'sfwerewriihuweruiwheruihuihhuhuhuhuihfuiwr')
+SECRET_KEY = os.environ.get("SECRET_KEY", 'sfwerewriihuweruiwheruihuihhuhuhuhuihfuiwr')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
